4b492482950084c101162da45091bf66a2e7c462
Aferro <aferro@unsa.edu.pe>
2023-07-07 19:25:26 -0500

    Cambio

--- Servicio/Service1.svc.cs
+++ Servicio/Service1.svc.cs
@@ -9,8 +9,6 @@ using Base_Datos.DAO;
 
 namespace Servicio
 {
-    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración a la vez.
-    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class Service1 : IService1
     {
         public IList<String> getCiudades()
